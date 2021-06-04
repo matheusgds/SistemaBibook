6f84ca882738d7674b4e3ecc9294939624b5cf21
matheus giacomelli dos santos <matheusgds66@gmail.com>
2021-06-04 15:53:24 -0300

    adicionado classe css, e as imagens
    adicionando as imagens para logotipo, para favicon, e criei a classe CSS para estilização

--- index.php
+++ index.php
@@ -7,7 +7,12 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <meta name="viewport" content="width-device-width,initial-scale=1.0"/>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="css/estilo.css"/>
+        <link rel="shortcut icon" href="IMG/livro32x32i.ico" >
+        <title>SISTEMA BIBOOK</title>
     </head>
     <body>
         <?php
