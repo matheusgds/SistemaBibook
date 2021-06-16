ae6af7e4171c4eac45211163ab2889fefd340ea9
matheus giacomelli dos santos <matheusgds66@gmail.com>
2021-06-16 00:02:20 -0300

    criado menu
    foi criado um menu responsivo do bootstrap, e organizado para forma do bibook

--- JanelaPrincipal.php
+++ JanelaPrincipal.php
@@ -18,23 +18,46 @@ and open the template in the editor.
     </head>
     <body>
         <div class="container-fluid">
-            <div class="pos-f-t">
-                <div class="collapse" id="navbarToggleExternalContent">
-                    <div class="bg-dark p-4">
-                        <h4 class="text-white">Collapsed content</h4>
-                        <span class="text-muted">Toggleable via the navbar brand.</span>
-                    </div>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="index.php">Sistema Bibook</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="JanelaPrincipal.php">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Dropdown
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </li>       
+
+                    </ul>
+                    <nav class="navbar navbar-light bg-light">
+                        <span>
+                            <img src="IMG/livro32x32p.png" width="30" height="30" class="d-inline-block align-top" alt="">
+                            BEM VINDO <?php /* $logado */ ?>
+                        </span>
+                    </nav> 
+
                 </div>
-                <nav class="navbar navbar-dark bg-dark">
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                </nav>
-            </div>
+            </nav>
 
 
             <div>
-                <?php /*   <img src="IMG/livro32x32p.png" width="30" height="30" class="d-inline-block align-top" alt="">
+                <?php /*
 
                   session_start();
 
