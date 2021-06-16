a3b61de7c6d86f2a83339d1a934c4e53e5335190
matheus giacomelli dos santos <matheusgds66@gmail.com>
2021-06-15 22:19:59 -0300

    acesso conexao por pdo
    foi criado um acesso de conexao ao banco de dados com pdo, e já setados os parametros de ligacao com o banco de dados

---
+++ confs/inc.php
@@ -0,0 +1,17 @@
+<?php
+
+    // Banco de Dados
+    define('HOST', 'localhost');  
+    define('DBNAME', 'sistemabibook');    
+    define('USER', 'root');  
+    define('PASSWORD', '');
+
+
+    
+    define('DRIVER', 'mysql'); 
+    define('CHARSET', 'utf8');
+
+    // Geral da Aplicação
+    define('NOME_DO_PROJETO','sistemabibook');
+    define('DESCRICAO_DO_PROJETO','Um Pequeno exemplo usando PDO');
+?>
