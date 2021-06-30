f3dea555a9a91679c85c01a9da34b78247b5bf3a
matheus giacomelli dos santos <matheusgds66@gmail.com>
2021-06-30 15:35:12 -0300

    atualizacao autoload
    criado o autoload para carregar as classes necessarias

--- autoload.php
+++ autoload.php
@@ -1,7 +1,7 @@
 <?php
 
 spl_autoload_register(function ($nomeClasse) {
-    if (file_exists("SistemaBibook" . DIRECTORY_SEPARATOR . "DTO" . DIRECTORY_SEPARATOR . $nomeClasse . ".class.php"))
-        require_once("SistemaBibook" . DIRECTORY_SEPARATOR . "DTO" . DIRECTORY_SEPARATOR . $nomeClasse . ".class.php");
+    if (file_exists(".." . DIRECTORY_SEPARATOR . "DTO" . DIRECTORY_SEPARATOR . $nomeClasse . ".class.php"))
+        require_once(".." . DIRECTORY_SEPARATOR . "DTO" . DIRECTORY_SEPARATOR . $nomeClasse . ".class.php");
 });
 ?>
